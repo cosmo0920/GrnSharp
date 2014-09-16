9f3caa1b9fa9f910a73274ed7aea1c1a0adaefd5
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-09-16 11:46:30 +0900

    add GrnUtil for utility methods

---
+++ GrnSharp/GrnUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class GrnUtil
+{
+	public static grn_user_data grn_ctx_user_data(ref grn_ctx ctx){
+		return ctx.user_data;
+	}
+
+	public static grn_encoding grn_ctx_get_encoding(ref grn_ctx ctx){
+		return ctx.encoding;
+	}
+
+	public static grn_encoding grn_ctx_set_encoding(ref grn_ctx ctx, string encoding){
+		return ctx.encoding = NativeMethods.grn_encoding_parse(encoding);
+	}
+}
